f5f37fad002459c7af7a889bc475550f43e899f3
mspaixao <mateus.paixao@labpix.com.br>
2019-07-01 01:40:13 -0300

    Middleaware de autenticação com jwt

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleaware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello Teste' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleaware);
+routes.put('/users', UserController.update);
 
 export default routes;
