13bbc00fa1e8f2de142d86d37f9b46db24b71bd3
mspaixao <mateus.paixao@labpix.com.br>
2019-07-05 13:51:24 -0300

    Adicionado multer para upload de arquivos, criado migration para upload de avata do provider

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleaware from './app/middlewares/auth';
 
 const routes = new Router();
+const upldoad = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleaware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upldoad.single('file'), FileController.store);
 
 export default routes;
